41ab4f66dcb75ad9cc4dcaca63de5a4df1bf2412
Dawit <dwtmekonnen123@gmail.com>
2021-12-14 11:28:24 +0300

    created schema and register and authentication

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
-
+const dotenv = require("dotenv");
+const userRoute = require("./routes/users");
+const AuthRoute = require("./routes/auth");
 const PORT = process.env.PORT || 5000;
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("db connection was successful");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+app.use(express.json());
+app.use("/api/v1/users", userRoute);
+app.use("/api/v1/auth", AuthRoute);
 
 app.listen(PORT, () => {
   console.log("server is running");

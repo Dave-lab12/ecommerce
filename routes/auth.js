fc15261c581e781b678f240ae8bf5a207dab2391
Dawit <dwtmekonnen123@gmail.com>
2021-12-16 14:36:14 +0300

    integrated jwt token and api endpoint for update, delete, and search

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const User = require("../models/User");
 const CryptoJS = require("crypto-js");
+const jwt = require("jsonwebtoken");
 //Register
 router.post("/register", async (req, res) => {
   //todo validate the input and send a 400 with a clear message
@@ -34,14 +35,28 @@ router.post("/login", async (req, res) => {
       user.password,
       process.env.PASS_SEC
     );
+
     const pass = hashedPassword.toString(CryptoJS.enc.Utf8);
+
     if (pass !== req.body.password) {
       return res.status(401).json({ message: "password is not correct" });
     }
+    const accessToken = jwt.sign(
+      {
+        id: user.id,
+        isAdmin: user.isAdmin,
+      },
+      process.env.JWT_SEC,
+      {
+        expiresIn: "3d",
+      }
+    );
+
     const { password, ...others } = user._doc;
-    res.status(200).json(others);
+
+    return res.status(200).json({ ...others, accessToken });
   } catch (err) {
-    res.status(500).json(err);
+    res.status(500).json({ message: "something went wrong" });
   }
 });
 
